2679c33d63d66f499766dff0de5a870914b733b4
Justin Borromeo <jborrome@edu.uwaterloo.ca>
2018-10-08 02:15:16 -0400

    Changed client

--- dev-client/client.py
+++ dev-client/client.py
@@ -1,30 +1,3 @@
 #!/usr/bin/env python3
 
-import asyncio
-import websockets
-
-async def hello():
-    async with websockets.connect(
-            'ws://localhost:8765') as websocket:
-        name = input("What's your name? ")
-
-        await websocket.send(name)
-        print(f"> {name}")
-
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
-
-async def goodbye():
-    async with websockets.connect(
-            'ws://localhost:8766') as websocket:
-        name = input("What's your name? ")
-
-        await websocket.send(name)
-        print(f"> {name}")
-
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
-
-asyncio.get_event_loop().run_until_complete(hello())
-
-asyncio.get_event_loop().run_until_complete(goodbye());
+# we should have some util functions for testing websocket stuff
